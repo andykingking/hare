76167cffa254d838f53c0e66fa15ed5300ca347d
Peter Hollows <peter.hollows@gmail.com>
2014-12-15 19:57:48 +1100

    View: Add some tests

---
+++ view_test.go
@@ -0,0 +1,26 @@
+package hare_test
+
+import (
+	. "github.com/captainpete/hare"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("View", func() {
+
+	var (
+		view *View
+	)
+
+	BeforeEach(func() {
+		view = &View{}
+	})
+
+	Describe("BucketName()", func() {
+		It("should be 'views' by default", func() {
+			Expect(view.BucketName()).To(Equal("views"))
+		})
+	})
+
+})
